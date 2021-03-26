fbcfd26d2a80aea1e6ea5ac869939f00571e7895
Life <iserje@icloud.com>
2021-03-26 18:46:19 +0600

    Add files via upload

---
+++ pkit.sh
@@ -0,0 +1,66 @@
+#!bin/bash
+#Coded by 7ife
+clear
+echo ""
+echo -e "\033[1;95m ‎"
+echo "${k}░█████╗░░██████╗██╗░█████╗░";
+echo "${k}██╔══██╗██╔════╝██║██╔══██╗";
+echo "${k}██║░░██║╚█████╗░██║███████║";
+echo "${k}██║░░██║░╚═══██╗██║██╔══██║";
+echo "${k}╚█████╔╝██████╔╝██║██║░░██║";
+echo "${k}░╚════╝░╚═════╝░╚═╝╚═╝░░╚═╝";
+echo ""
+echo -e "\033[1;90m  Github: github.com/7ife"
+echo -e "\033[1;90m  E-mail: 7ife@pm.me"
+echo ""
+echo -e "\033[1;94m::onion site in android::\e[0m \e[1;35mPagekit CMS\e[0m 1.0.18"
+sleep 2
+echo ""
+echo -e ""
+echo -e $'\e[1;34m::osia::\e[0m\e[1;32m #Installing Dependencies\e[0m'
+sleep 2
+apt install wget -y
+apt install apache2 -y
+apt install php -y
+apt install php-apache -y
+apt install tor -y
+cd $PREFIX/etc/apache2/
+rm httpd.conf
+wget https://raw.githubusercontent.com/7ife/7ife.github.io/master/data/httpd.zip
+unzip httpd.zip
+rm httpd.zip
+cd $PREFIX/share/apache2/
+wget https://raw.githubusercontent.com/7ife/7ife.github.io/master/data/pagekit/site.tar.gz
+tar -xvpf site.tar.gz
+rm site.tar.gz
+cd $PREFIX/var/
+mkdir run
+echo ""
+echo -e $'\e[1;34m::osia::\e[0m\e[1;32m #Starting Apache2\e[0m'
+sleep 2
+apachectl start
+echo -e " \033[1;93m Apache2 is up"
+echo ""
+sleep 1
+echo ""
+echo -e $'\e[1;34m::osia::\e[0m\e[1;32m #Starting TOR\e[0m'
+sleep 2
+cd $PREFIX/share/apache2/site/onion/
+tor -f instance_torrc &
+sleep 33
+cd $PREFIX/share/apache2/site/onion/tor-dm
+echo ""
+echo -e $'\e[1;34m::osia::\e[0m\e[1;32m #Your Onion domain for Pagekit is Ready\e[0m'
+echo ""
+echo -e "\e[91m "
+cat hostname
+echo -e "\e[39m "
+echo ""
+echo -e "\033[1;33m If Bootstrapped 100% after 15-20 minutes Can go to Your address in the Tor Browser"
+echo ""
+echo -e $'\e[1;34m::osia::\e[0m\033[1;90m #Press CTRL + C to close Tor\e[0m'
+echo ""
+while true
+do
+sleep 5
+done
